47427fce151b8f7d09758714eb45e8df513a8cd4
grant <fishkq@aliyun.com>
2020-05-14 18:37:19 +0800

    1. [优化] mybatis 测试案例

--- gt-core-base-spring2-mybatis-test/src/test/java/com/github/cooker/AppTest.java
+++ gt-core-base-spring2-mybatis-test/src/test/java/com/github/cooker/AppTest.java
@@ -3,12 +3,20 @@ package com.github.cooker;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.servlet.SingleThreadModel;
 
 /**
  * Unit test for simple App.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = MybatisApp.class)
 public class AppTest 
 {
     /**
